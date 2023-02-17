7952a820b5a94446aa3e49ef3ef97da9dc636b88
nikitaantsev <71016018+nikitaantsev@users.noreply.github.com>
2023-02-18 00:51:26 +0300

    Add files via upload

---
+++ tex_lab.8/tex_lab.8/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace tex_lab._8
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размер матрицы:");
+            int N = int.Parse(Console.ReadLine());
+            int M = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[N, M];
+            Random rand = new Random();
+            int[] summ = new int[M];
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+                    matrix[i, j] = rand.Next(0, 10); //рандомные значения для елементов матрицы
+
+                }
+            }
+            Console.WriteLine("Матрица");
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+
+                    Console.Write(matrix[i, j]); //вывод матрицы
+                    summ[i] += matrix[j, i];      //подсчет суммы колонки
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            foreach (double elem in summ)
+            {
+
+                Console.WriteLine(elem / M); //вывод среднего для колонки
+            }
+
+        }
+    }
+}
